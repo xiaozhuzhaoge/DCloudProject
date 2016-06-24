17a114632215935d7aa200454dc6cd9d855a8409
xiaozhuzhaoge <xiaozhuzhaoge@gmail.com>
2016-06-24 14:38:26 +0800

    Merge connection methods into Connect folder.

--- QinCloud/js/HttpCore/HttpBase.js
+++ QinCloud/js/HttpCore/HttpBase.js
@@ -1,15 +1,29 @@
-function postData(url, form, callback ) {  
-      
-    mui.ajax(url,{  
-        data: form,
-        dataType:'json',  
-        type:'post',  
-        contentType:"application/x-www-form-urlencoded; charset=utf-8",  
-        timeout:60000,  
-        success:callback,  
-        error:function(xhr,type,errorThrown){  
-        	 
-            mui.alert("<网络连接失败，请重新尝试一下>", "错误", "OK", null);  
-        }  
-    });  
-}  
+
+/*
+ * Http Post
+ * uri 发送请求URI
+ * pre uri后续内容
+ * sendfrom 发送表单
+ * success success回调
+ * fail 失败回调
+ */
+function PostData(uri, pre, sendfrom, success, fail) {
+
+	var oReq = new XMLHttpRequest();
+	oReq.open("POST", uri);
+	oReq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	var jsond = JSON.stringify(sendfrom);
+
+	oReq.send(pre + jsond.toString());
+	oReq.onreadystatechange = function() {
+		if (oReq.readyState == 4) {
+			var response = $.parseJSON(oReq.responseText);
+			var result = response['' + "result" + ''];
+			if (result == 0) {
+				success(response);
+			} else {
+				fail(response);
+			}
+		}
+	}
+}
